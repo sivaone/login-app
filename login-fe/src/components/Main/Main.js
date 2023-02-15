ce077dcf49e1a51b3839f7512d6e34e57769a5b0
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2023-02-14 20:32:22 -0600

    Add logic to submit form data

--- login-fe/src/components/Main/Main.js
+++ login-fe/src/components/Main/Main.js
@@ -1,40 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
-import "./main.css"
+import "./main.css";
+
+async function generatePassword(profile) {
+    return fetch('http://localhost:8080/generate-password', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(profile)
+    })
+        .then(data => data.json());
+};
 
 export default function Main() {
+
+    const [domain, setDomain] = useState();
+    const [lowercase, setLowercase] = useState();
+    const [uppercase, setUppercase] = useState();
+    const [digits, setDigits] = useState();
+    const [symbols, setSymbols] = useState();
+    const [excludedChars, setExcludedChars] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const pwd = await generatePassword({
+            domain,
+            lowercase,
+            uppercase,
+            digits,
+            symbols,
+            excludedChars
+        });
+
+    };
+
+
     return (
         <div className="form-wrapper">
             <h2>Main</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
-                    <input type="text" placeholder="Enter domain"/>
-                </label><br/>
+                    <input type="text" placeholder="Enter domain" onChange={e => setDomain(e.target.value)} />
+                </label><br />
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={e => setLowercase(e.target.value)} />
                     <span> Include Lowercase</span>
-                </label><br/>
+                </label><br />
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={e => setUppercase(e.target.value)} />
                     <span> Include Uppercase</span>
-                </label><br/>
+                </label><br />
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={e => setDigits(e.target.value)} />
                     <span> Include Digits</span>
-                </label><br/>
+                </label><br />
                 <label>
-                    <input type="checkbox" />
-                    <span> Include Symbols (!@#$%^&*()_+-=[]{}|,.&LT;&GT;/?\"';:)</span>
-                </label><br/>
+                    <input type="checkbox" onChange={e => setSymbols(e.target.value)} />
+                    <span> Include Symbols (!@#$%^&*()_+-=[]{ }|,.&LT;&GT;/?\"';:)</span>
+                </label><br />
                 <label>
                     <span>Exclude Characters : </span>
-                    <input type="text" />
+                    <input type="text" onChange={e => setExcludedChars(e.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Generate</button>
                 </div>
                 <label>
-                    <input type="text" placeholder="Generated password"/>
+                    <input type="text" placeholder="Generated password" />
                     <button>Copy</button>
                 </label>
             </form>
--- login-fe/src/components/Profile/Profile.js
+++ login-fe/src/components/Profile/Profile.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
-import "./profile.css"
+import "./profile.css";
+
+
+async function saveProfile(profile) {
+    return fetch('http://localhost:8080/profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(profile)
+    })
+        .then(data => data.json());
+};
 
 export default function Profile() {
+
+    const [lowercase, setLowercase] = useState();
+    const [uppercase, setUppercase] = useState();
+    const [digits, setDigits] = useState();
+    const [symbols, setSymbols] = useState();
+    const [excludedChars, setExcludedChars] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const resp = await saveProfile({
+            lowercase,
+            uppercase,
+            digits,
+            symbols,
+            excludedChars
+        });
+
+    };
+
     return (
         <div className="form-wrapper">
             <h2>Profile</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={e => setLowercase(e.target.value)} />
                     <span> Include Lowercase</span>
-                </label><br/>
+                </label><br />
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={e => setUppercase(e.target.value)} />
                     <span> Include Uppercase</span>
-                </label><br/>
+                </label><br />
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={e => setDigits(e.target.value)} />
                     <span> Include Digits</span>
-                </label><br/>
+                </label><br />
                 <label>
-                    <input type="checkbox" />
-                    <span> Include Symbols (!@#$%^&*()_+-=[]{}|,.&LT;&GT;/?\"';:)</span>
-                </label><br/>
+                    <input type="checkbox" onChange={e => setSymbols(e.target.value)} />
+                    <span> Include Symbols (!@#$%^&*()_+-=[]{ }|,.&LT;&GT;/?\"';:)</span>
+                </label><br />
                 <label>
                     <span>Exclude Characters : </span>
-                    <input type="text" />
+                    <input type="text" onChange={e => setExcludedChars(e.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
